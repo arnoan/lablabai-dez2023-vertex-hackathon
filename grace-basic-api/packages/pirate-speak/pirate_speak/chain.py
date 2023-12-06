80c36cf5b77dadaa897c3c01d39174bbfcd6bb2d
Arno Angerer <angerer.arno@gmail.com>
2023-12-06 13:59:34 +0100

    feat: modify chain to take multiple inputs, also use gpt-4 instead of default (gpt-3-turbo)

--- grace-basic-api/packages/pirate-speak/pirate_speak/chain.py
+++ grace-basic-api/packages/pirate-speak/pirate_speak/chain.py
@@ -3,14 +3,15 @@ from langchain.prompts import ChatPromptTemplate
 
 _prompt = ChatPromptTemplate.from_messages(
     [
-        (
-            "system",
-            "Translate user input into pirate speak",
-        ),
-        ("human", "{text}"),
+        # (
+        #     "system",
+        #     "Tell a joke. An",
+        # ),
+        ("human", "Tell me a joke about {topic}. "
+                  "And answer me in {language}, even if my question is in a different language."),
     ]
 )
-_model = ChatOpenAI()
+_model = ChatOpenAI(model="gpt-4")
 
 # if you update this, you MUST also update ../pyproject.toml
 # with the new `tool.langserve.export_attr`
